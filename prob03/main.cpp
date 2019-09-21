a16325b9154c9ebd1e3e7b3c52611561fb5534f0
m3aldonado <m3aldonado@cs104u-6g.acad.fullerton.edu>
2019-09-21 14:33:54 -0700

    random rectangle generator

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,24 @@
 // Random Rectangle
+\
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+int main()
+{
+
+const int SIZE = 40;
+
+//setting character as # for rectangle
+
+std::string length;
+unsigned seed = time(0);
+srand(seed);
+length.assign ((rand() % SIZE + 1), '#');
+std::cout << length + "\n" + length + "\n" + length << std::endl;
+
+
+
+
+return 0;
+}
